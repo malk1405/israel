d9fba1dec8523ffb1367a9310118fcf397d6db3c
malk <malk1405@mail.ru>
2020-04-27 01:58:43 +0300

    Наполнил текстовое содержимое раздела Как поехать

--- gulpfile.js
+++ gulpfile.js
@@ -1,156 +1,157 @@
-'use strict';
 
-var gulp = require('gulp');
-var plumber = require('gulp-plumber');
-var sourcemap = require('gulp-sourcemaps');
-var sass = require('gulp-sass');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var server = require('browser-sync').create();
-var csso = require('gulp-csso');
-var rename = require('gulp-rename');
-var imagemin = require('gulp-imagemin');
-var webp = require('gulp-webp');
-var svgstore = require('gulp-svgstore');
-var posthtml = require('gulp-posthtml');
-var include = require('posthtml-include');
-var del = require('del');
 
-var webpack = require('webpack-stream');
+let gulp = require(`gulp`);
+let plumber = require(`gulp-plumber`);
+let sourcemap = require(`gulp-sourcemaps`);
+let sass = require(`gulp-sass`);
+let postcss = require(`gulp-postcss`);
+let autoprefixer = require(`autoprefixer`);
+let server = require(`browser-sync`).create();
+let csso = require(`gulp-csso`);
+let rename = require(`gulp-rename`);
+let imagemin = require(`gulp-imagemin`);
+let webp = require(`gulp-webp`);
+let svgstore = require(`gulp-svgstore`);
+let posthtml = require(`gulp-posthtml`);
+let include = require(`posthtml-include`);
+let del = require(`del`);
 
-gulp.task('css', function () {
+let webpack = require(`webpack-stream`);
+
+gulp.task(`css`, function () {
   return gulp
-    .src('source/sass/style.scss')
-    .pipe(plumber())
-    .pipe(sourcemap.init())
-    .pipe(sass())
-    .pipe(postcss([autoprefixer()]))
-    .pipe(gulp.dest('build/css'))
-    .pipe(csso())
-    .pipe(rename('style.min.css'))
-    .pipe(sourcemap.write('.'))
-    .pipe(gulp.dest('build/css'))
-    .pipe(server.stream());
+      .src(`source/sass/style.scss`)
+      .pipe(plumber())
+      .pipe(sourcemap.init())
+      .pipe(sass())
+      .pipe(postcss([autoprefixer()]))
+      .pipe(gulp.dest(`build/css`))
+      .pipe(csso())
+      .pipe(rename(`style.min.css`))
+      .pipe(sourcemap.write(`.`))
+      .pipe(gulp.dest(`build/css`))
+      .pipe(server.stream());
 });
 
-gulp.task('server', function () {
+gulp.task(`server`, function () {
   server.init({
-    server: 'build/',
+    server: `build/`,
     notify: false,
     open: true,
     cors: true,
     ui: false,
   });
 
-  gulp.watch('source/sass/**/*.{scss,sass}', gulp.series('css'));
-  gulp.watch('source/img/icon-*.svg', gulp.series('sprite', 'html', 'refresh'));
-  gulp.watch('source/*.html', gulp.series('html', 'refresh'));
-  gulp.watch('source/js/**/*.js', gulp.series('js', 'refresh'));
-  gulp.watch('source/img/**/*.{png,jpg,svg}', gulp.series('copy', 'refresh'));
+  gulp.watch(`source/sass/**/*.{scss,sass}`, gulp.series(`css`));
+  gulp.watch(`source/img/icon-*.svg`, gulp.series(`sprite`, `html`, `refresh`));
+  gulp.watch(`source/*.html`, gulp.series(`html`, `refresh`));
+  gulp.watch(`source/js/**/*.js`, gulp.series(`js`, `refresh`));
+  gulp.watch(`source/img/**/*.{png,jpg,gif}`, gulp.series(`copy`, `refresh`));
+  gulp.watch(`source/img/**/*.svg`, gulp.series(`copy`, `sprite`));
 });
 
-gulp.task('js', () => {
-  const entry = ['main', 'vendor'].reduce((acc, el) => {
+gulp.task(`js`, () => {
+  const entry = [`main`, `vendor`].reduce((acc, el) => {
     acc[el] = `./source/js/${el}.js`;
     return acc;
   }, {});
 
   return gulp
-    .src('./source/js/')
-    .pipe(
-      webpack({
-        entry,
-        mode: 'development',
-        output: {
-          filename: '[name].js',
-        },
-        watch: false,
-        devtool: 'source-map',
-        module: {
-          rules: [
-            {
-              test: /\.m?js$/,
-              exclude: /(node_modules|bower_components)/,
-              use: {
-                loader: 'babel-loader',
-                options: {
-                  presets: [
-                    [
-                      '@babel/preset-env',
-                      {
-                        debug: true,
-                        corejs: 3,
-                        useBuiltIns: 'usage',
-                      },
-                    ],
-                  ],
-                  plugins: [
-                    '@babel/plugin-transform-runtime',
-                    '@babel/plugin-transform-async-to-generator',
-                  ],
-                },
-              },
+      .src(`./source/js/`)
+      .pipe(
+          webpack({
+            entry,
+            mode: `development`,
+            output: {
+              filename: `[name].js`,
             },
-          ],
-        },
-      })
-    )
-    .pipe(gulp.dest('./build/js'));
+            watch: false,
+            devtool: `source-map`,
+            module: {
+              rules: [
+                {
+                  test: /\.m?js$/,
+                  exclude: /(node_modules|bower_components)/,
+                  use: {
+                    loader: `babel-loader`,
+                    options: {
+                      presets: [
+                        [
+                          `@babel/preset-env`,
+                          {
+                            debug: true,
+                            corejs: 3,
+                            useBuiltIns: `usage`,
+                          },
+                        ],
+                      ],
+                      plugins: [
+                        `@babel/plugin-transform-runtime`,
+                        `@babel/plugin-transform-async-to-generator`,
+                      ],
+                    },
+                  },
+                },
+              ],
+            },
+          })
+      )
+      .pipe(gulp.dest(`./build/js`));
 });
 
-gulp.task('refresh', function (done) {
+gulp.task(`refresh`, function (done) {
   server.reload();
   done();
 });
 
-gulp.task('images', function () {
+gulp.task(`images`, function () {
   return gulp
-    .src('source/img/**/*.{svg}')
-    .pipe(imagemin([imagemin.svgo()]))
+      .src(`source/img/**/*.{svg}`)
+      .pipe(imagemin([imagemin.svgo()]))
 
-    .pipe(gulp.dest('source/img'));
+      .pipe(gulp.dest(`source/img`));
 });
 
-gulp.task('webp', function () {
+gulp.task(`webp`, function () {
   return gulp
-    .src('source/img/**/*.{png,jpg}')
-    .pipe(webp({ quality: 90 }))
-    .pipe(gulp.dest('source/img'));
+      .src(`source/img/**/*.{png,jpg}`)
+      .pipe(webp({quality: 90}))
+      .pipe(gulp.dest(`source/img`));
 });
 
-gulp.task('sprite', function () {
+gulp.task(`sprite`, function () {
   return gulp
-    .src('source/img/svg/*.svg')
-    .pipe(svgstore({ inlineSvg: true }))
-    .pipe(rename('sprite.svg'))
-    .pipe(gulp.dest('build/img/svg'));
+      .src(`source/img/svg/*.svg`)
+      .pipe(svgstore({inlineSvg: true}))
+      .pipe(rename(`sprite.svg`))
+      .pipe(gulp.dest(`build/img/svg`));
 });
 
-gulp.task('html', function () {
+gulp.task(`html`, function () {
   return gulp
-    .src('source/*.html')
-    .pipe(posthtml([include()]))
-    .pipe(gulp.dest('build'));
+      .src(`source/*.html`)
+      .pipe(posthtml([include()]))
+      .pipe(gulp.dest(`build`));
 });
 
-gulp.task('copy', function () {
+gulp.task(`copy`, function () {
   return gulp
-    .src(
-      [
-        'source/fonts/**/*.{woff,woff2}',
-        'source/img/**/*.{webp,jpg,png,svg}',
-        'source//*.ico',
-      ],
-      {
-        base: 'source',
-      }
-    )
-    .pipe(gulp.dest('build'));
+      .src(
+          [
+            `source/fonts/**/*.{woff,woff2}`,
+            `source/img/**/*.{webp,jpg,png,gif,svg}`,
+            `source//*.ico`,
+          ],
+          {
+            base: `source`,
+          }
+      )
+      .pipe(gulp.dest(`build`));
 });
 
-gulp.task('clean', function () {
-  return del('build');
+gulp.task(`clean`, function () {
+  return del(`build`);
 });
 
-gulp.task('build', gulp.series('clean', 'copy', 'css', 'js', 'sprite', 'html'));
-gulp.task('start', gulp.series('build', 'server'));
+gulp.task(`build`, gulp.series(`clean`, `copy`, `css`, `js`, `sprite`, `html`));
+gulp.task(`start`, gulp.series(`build`, `server`));
