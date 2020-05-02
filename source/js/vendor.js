72d239305c0be36c1227a5f90b442e9d47657d51
malk <malk1405@mail.ru>
2020-05-02 11:06:25 +0300

    Подключил imask.js

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,7 +1,10 @@
 import picturefill from 'picturefill';
 import svg4everybody from 'svg4everybody';
+import IMask from "imask";
 
 (function vendors() {
   svg4everybody();
   picturefill();
+
+  window.IMask = IMask;
 })();
