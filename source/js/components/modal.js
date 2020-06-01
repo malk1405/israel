40f899bdb707fc6e56f59048956e01ab461aa97c
malk1405 <malk1405@mail.ru>
2020-06-01 12:29:59 +0300

    Добавил закрытие модалки по нажатию на esc

--- source/js/components/modal.js
+++ source/js/components/modal.js
@@ -14,10 +14,33 @@ function createModal(el) {
   const modalContainer = clone.querySelector(`.modal-overlay`);
   const modal = modalContainer.querySelector(`.modal`);
   const closeBtn = modal.querySelector(`.modal__close-btn`);
-  closeBtn.addEventListener(`click`, destroy);
+
+  modalContainer.addEventListener(`click`, onClick);
 
   document.body.appendChild(modalContainer);
+  document.addEventListener(`keydown`, onEscape);
+
+  function onClick(e) {
+    const targets = [modalContainer, closeBtn];
+
+    for (let i = 0; i < targets.length; i++) {
+      if (e.target === targets[i]) {
+        destroy();
+        return;
+      }
+    }
+  }
+
+  function onEscape(e) {
+    if (e.keyCode === 27) {
+      destroy();
+    }
+  }
+
   function destroy() {
+    document.removeEventListener(`keydown`, onEscape);
+    modalContainer.removeEventListener(`click`, onClick);
+
     document.body.removeChild(modalContainer);
   }
 }
