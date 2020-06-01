00f65fee0a4710dd125b4cab36d4ac2be4b18f18
malk1405 <malk1405@mail.ru>
2020-06-01 21:33:08 +0300

    Отрефакторил модуль под es6

--- source/js/components/form/input.js
+++ source/js/components/form/input.js
@@ -1,20 +1,15 @@
+import setListeners from '../../utils/setListeners';
+
 function activateInput(input) {
   const className = `input--not-touched`;
+  const listener = setListeners({elements: [input], events: [`focus`, `invalid`], fn: onEvent});
+
   input.classList.add(className);
-
-  const events = [`focus`, `invalid`];
-
-  toggleListeners(true);
+  listener.add();
 
   function onEvent() {
     input.classList.remove(className);
-    toggleListeners(false);
-  }
-
-  function toggleListeners(condition) {
-    for (let i = 0; i < events.length; i++) {
-      input[`${condition ? `add` : `remove`}EventListener`](events[i], onEvent);
-    }
+    listener.remove();
   }
 }
 
